ae626055e7ebf8120d03bbaf51e701e9e2299d47
Michael Payne <10254938+michaelpayne02@users.noreply.github.com>
2024-04-23 21:38:12 -0500

    feat: page search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,30 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
-import remarkUnwrapImages from 'remark-unwrap-images'
-import rehypeExternalLinks from 'rehype-external-links'
-import expressiveCode from 'astro-expressive-code'
-import { expressiveCodeOptions } from './src/site.config'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts';
+import remarkUnwrapImages from 'remark-unwrap-images';
+import rehypeExternalLinks from 'rehype-external-links';
+import expressiveCode from 'astro-expressive-code';
+import { expressiveCodeOptions } from './src/site.config';
+import icon from 'astro-icon';
+import vercel from '@astrojs/vercel/serverless';
+import lottie from 'astro-integration-lottie';
 
-import vercel from '@astrojs/vercel/serverless'
-import lottie from 'astro-integration-lottie'
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.me',
-  integrations: [
-    expressiveCode(expressiveCodeOptions),
-    tailwind({
-      applyBaseStyles: false
-    }),
-    sitemap(),
-    lottie(),
-    mdx(),
-    icon()
-  ],
+  integrations: [expressiveCode(expressiveCodeOptions), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), lottie(), mdx(), icon()],
   markdown: {
     remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: '_blank',
-          rel: ['nofollow, noopener, noreferrer']
-        }
-      ]
-    ],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank',
+      rel: ['nofollow, noopener, noreferrer']
+    }]],
     remarkRehype: {
       footnoteLabelProperties: {
         className: ['']
@@ -43,8 +32,12 @@ export default defineConfig({
     }
   },
   prefetch: true,
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
+  output: 'server'
+  // adapter: vercel({
+  //   webAnalytics: { enabled: true }
+  // })
+  ,
+  adapter: node({
+    mode: "standalone"
   })
-})
+});
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,16 +1,3 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro-integration-lottie/env" />
-declare module "@pagefind/default-ui" {
-	declare class PagefindUI {
-		constructor(arg: unknown);
-	}
-}
-
-interface ImportMetaEnv {
-	readonly WEBMENTION_API_KEY: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
