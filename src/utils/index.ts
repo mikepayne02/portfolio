d1772a0749166c053f322fe67d14945617ed6e57
Michael Payne <10254938+michaelpayne02@users.noreply.github.com>
2024-04-27 02:01:56 -0500

    fix: reader view images
    feat: lightbox image component

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
+import LightboxImage from '@components/LightboxImage.astro'
 export { cn } from './tailwind'
 export { getAllPosts, sortMDByDate, getUniqueTags, getUniqueTagsWithCount } from './project'
 export { getFormattedDate } from './date'
 export { generateToc } from './generateToc'
+export { LightboxImage as Lightbox }
 export type { TocItem } from './generateToc'
